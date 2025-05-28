53570c73a6f413eb56c47e3b485942494f205917
Vladis-Vdroid <vasilev.vladimir@yahoo.com>
2025-05-28 16:22:09 +0300

    LoginDto

---
+++ DTO's/LoginDto.cs
@@ -0,0 +1,7 @@
+namespace APIDatingApp.DTO_s;
+
+public class LoginDto
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
--- obj/Debug/net9.0/APIDatingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/APIDatingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIDatingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17e8aa46c13f3998de2ae843c670de6bffe086b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff35edf2a1053fae3fc809e6f66862a71c4d62e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIDatingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIDatingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
